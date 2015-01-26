28a4978ae0ae5cb63458d9827009613526a9c971
Michael Alt <lean@michael-alt.info>
2015-01-26 11:13:31 +0100

    Adding browser-sync to the workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 
 var gulp = require('gulp');
 
-var wiredep = require('wiredep').stream;
+var wiredep     = require('wiredep').stream;
+var browserSync = require('browser-sync');
+
+var srcDir = __dirname + '/src/';
 
 gulp.task('wiredep',function(){
-  return gulp.src('src/_*/*.html')
+  return gulp.src(srcDir + '_*/*.html')
     .pipe(wiredep())
-    .pipe(gulp.dest('src/'));
+    .pipe(gulp.dest(srcDir));
+});
+
+gulp.task('pages', function(){
+  
+});
+
+gulp.task('dev', function(){
+  browserSync({ server: { baseDir: srcDir }});
 });
